e42f409fbf7184d7038dad4f97bb14ea3477e752
Arfaniel <dn120284aio@gmail.com>
2016-12-03 16:48:32 +0200

    registration is done

--- LoginForm_WinForm/Form1.Designer.cs
+++ LoginForm_WinForm/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LoginForm_WinForm
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LoginBox = new System.Windows.Forms.TextBox();
+            this.PassBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SignInButton = new System.Windows.Forms.Button();
+            this.RegisterButton = new System.Windows.Forms.Button();
+            this.PassProgress = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // LoginBox
+            // 
+            this.LoginBox.Location = new System.Drawing.Point(64, 59);
+            this.LoginBox.MaxLength = 10;
+            this.LoginBox.Name = "LoginBox";
+            this.LoginBox.Size = new System.Drawing.Size(159, 20);
+            this.LoginBox.TabIndex = 0;
+            // 
+            // PassBox
+            // 
+            this.PassBox.Location = new System.Drawing.Point(64, 111);
+            this.PassBox.MaxLength = 8;
+            this.PassBox.Name = "PassBox";
+            this.PassBox.Size = new System.Drawing.Size(159, 20);
+            this.PassBox.TabIndex = 1;
+            this.PassBox.TextChanged += new System.EventHandler(this.PassBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // SignInButton
+            // 
+            this.SignInButton.Location = new System.Drawing.Point(67, 172);
+            this.SignInButton.Name = "SignInButton";
+            this.SignInButton.Size = new System.Drawing.Size(156, 23);
+            this.SignInButton.TabIndex = 4;
+            this.SignInButton.Text = "SignIn";
+            this.SignInButton.UseVisualStyleBackColor = true;
+            this.SignInButton.Click += new System.EventHandler(this.SignInButton_Click);
+            // 
+            // RegisterButton
+            // 
+            this.RegisterButton.Location = new System.Drawing.Point(67, 203);
+            this.RegisterButton.Name = "RegisterButton";
+            this.RegisterButton.Size = new System.Drawing.Size(156, 23);
+            this.RegisterButton.TabIndex = 5;
+            this.RegisterButton.Text = "Register";
+            this.RegisterButton.UseVisualStyleBackColor = true;
+            this.RegisterButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PassProgress
+            // 
+            this.PassProgress.Location = new System.Drawing.Point(64, 138);
+            this.PassProgress.Maximum = 8;
+            this.PassProgress.Name = "PassProgress";
+            this.PassProgress.Size = new System.Drawing.Size(159, 10);
+            this.PassProgress.TabIndex = 6;
+            // 
+            // LoginForm
+            // 
+            this.AcceptButton = this.RegisterButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.PassProgress);
+            this.Controls.Add(this.RegisterButton);
+            this.Controls.Add(this.SignInButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PassBox);
+            this.Controls.Add(this.LoginBox);
+            this.Name = "LoginForm";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox LoginBox;
+        private System.Windows.Forms.TextBox PassBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SignInButton;
+        private System.Windows.Forms.Button RegisterButton;
+        private System.Windows.Forms.ProgressBar PassProgress;
     }
 }
 
--- LoginForm_WinForm/Form1.cs
+++ LoginForm_WinForm/Form1.cs
@@ -7,14 +7,92 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LoginForm_WinForm
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private bool isSuchLogin(string login)
+        {
+            string[] allUsers;
+            using (StreamReader fs = new StreamReader("users.txt"))
+            {
+                allUsers = fs.ReadToEnd().Split();
+            }
+            foreach (string word in allUsers)
+            {
+                if (word == login)
+                {
+                    return true;
+                }
+            }
+            return false;     
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(LoginBox.Text))
+            {
+                MessageBox.Show("Enter login");
+            }
+            if(String.IsNullOrEmpty(PassBox.Text))
+            {
+                MessageBox.Show("Enter password");
+            }
+            else
+            {
+                if (PassBox.Text.Length<5)
+                {
+                    MessageBox.Show("Password is too short");
+                    PassBox.Text = "";
+                    return;
+                }
+
+                if (isSuchLogin(LoginBox.Text)== true)
+                {
+                    MessageBox.Show("Such user already exists");
+                    LoginBox.Text = "";
+                    PassBox.Text = "";
+                    return;
+                }
+                if (isSuchLogin(LoginBox.Text) == false)
+                {
+                    using (StreamWriter sw = new StreamWriter("users.txt", true))
+                    {
+                        sw.WriteLine();
+                        sw.Write(LoginBox.Text);
+                        sw.Write(" ");
+                        sw.Write(PassBox.Text);
+                    }
+                    MessageBox.Show("Registration completed successfully");
+                    LoginBox.Text = "";
+                    PassBox.Text = "";
+                }
+            }
+        }
+
+        private void PassBox_TextChanged(object sender, EventArgs e)
+        {
+            PassProgress.Value = PassBox.Text.Length;
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            if (isSuchLogin(LoginBox.Text)==false)
+            {
+                MessageBox.Show("Login or password is incorrect");
+                return;
+            }
+            else
+            {
+
+            }
         }
     }
 }
